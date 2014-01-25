c46790e665dbe79d85c13905bff1c409039f84aa
Deneos <christophe.galati@gmail.com>
2014-01-25 16:17:48 +0100

    LD
    modifier le spawn

--- scripts/class/level.js
+++ scripts/class/level.js
@@ -3,7 +3,7 @@ var Level = function()
     this.decount = 0;
     this.init = function()
     {
-        game.crowd.tabPeople.push(new People({ slot : game.crowd.slot[0], nature : 'bad', speed : 0.2}));
+        /*game.crowd.tabPeople.push(new People({ slot : game.crowd.slot[0], nature : 'bad', speed : 0.2}));
         game.crowd.nb_of_bad++;
         game.crowd.slot[0].occupated = true;
         game.crowd.tabPeople.push(new People({ slot : game.crowd.slot[1], nature : 'nice', speed : 0.2}));
@@ -23,47 +23,13 @@ var Level = function()
         game.crowd.slot[5].occupated = true;
         game.crowd.tabPeople.push(new People({ slot : game.crowd.slot[6], nature : 'nice', speed : 0.2}));
         game.crowd.nb_of_nice++;
-        game.crowd.slot[6].occupated = true;
+        game.crowd.slot[6].occupated = true;*/
     }
     this.motor = function()
     {
         this.decount++;
-        if(this.decount%100===0)
+        if(this.decount%100===0 && (game.crowd.nb_of_bad+game.crowd.nb_of_nice) < 12)
         {   
-            //bien
-            if(game.player.assurance >= 40 && game.player.assurance <= 60)
-            {
-                var alea = Math.floor(Math.random()*7);
-                var aleaNature = Math.round(Math.random()*3);
-                if(aleaNature===3)
-                {    
-                    var carac = 'bad';
-                    game.crowd.nb_of_bad++;
-                }
-                else
-                {    
-                    var carac = 'nice';
-                    game.crowd.nb_of_nice++;
-                }
-                game.crowd.tabPeople.push(new People({ slot : game.crowd.slot[alea], nature : carac, speed : 0.2}));
-            }
-            //plutot bien
-            if(game.player.assurance >= 20 && game.player.assurance <= 40)
-            {
-                var alea = Math.floor(Math.random()*7);
-                var aleaNature = Math.round(Math.random()*2);
-                if(aleaNature===2)
-                {    
-                    var carac = 'bad';
-                    game.crowd.nb_of_bad++;
-                }
-                else
-                {    
-                    var carac = 'nice';
-                    game.crowd.nb_of_nice++;
-                }
-                game.crowd.tabPeople.push(new People({ slot : game.crowd.slot[alea], nature : carac, speed : 0.2}));
-            }
             //neutre
             if(game.player.assurance >= 40 && game.player.assurance <= 60)
             {
@@ -81,6 +47,44 @@ var Level = function()
                 }
                 game.crowd.tabPeople.push(new People({ slot : game.crowd.slot[alea], nature : carac, speed : 0.2}));
             }
+
+            //bien
+            if(game.player.assurance >= 0 && game.player.assurance < 40)
+            {
+                var alea = Math.floor(Math.random()*7);
+                var aleaNature = Math.round(Math.random());
+                if(aleaNature===1)
+                {    
+                    var carac = 'bad';
+                    game.crowd.nb_of_bad++;
+                }
+                else
+                {    
+                    var carac = 'nice';
+                    game.crowd.nb_of_nice++;
+                }
+                game.crowd.tabPeople.push(new People({ slot : game.crowd.slot[alea], nature : carac, speed : 0.2}));
+            }
+
+            //mal
+            if(game.player.assurance > 60 && game.player.assurance <= 100)
+            {
+                var alea = Math.floor(Math.random()*7);
+                var aleaNature = Math.round(Math.random()*2);
+                if(aleaNature===1)
+                {    
+                    var carac = 'nice';
+                    game.crowd.nb_of_nice++;
+                }
+                else
+                {    
+                    var carac = 'bad';
+                    game.crowd.nb_of_bad++;
+                }
+                game.crowd.tabPeople.push(new People({ slot : game.crowd.slot[alea], nature : carac, speed : 0.2}));
+            }
+
+
             
         }
     }
