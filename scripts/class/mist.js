e7610a185d15b184da99d0bab68d7ec139233260
Deneos <christophe.galati@gmail.com>
2014-01-25 18:06:34 +0100

    Revert "color change"
    This reverts commit d9b23428605afae0e147105a81889f384d1989df.

--- scripts/class/mist.js
+++ scripts/class/mist.js
@@ -1,10 +1,7 @@
 var Mist = function()
 {
-    this.background0 = new Image();
-    this.background1 = new Image();
-    this.background2 = new Image();
-
-    
+    this.background = new Image();
+    this.background.src = "medias/images/decor.png";
     this.init = function()
     {
         var grd = contextBuffer.createRadialGradient(canvasWidth/2,canvasHeight/2,5,90,60,canvasWidth);
@@ -14,43 +11,10 @@ var Mist = function()
         // Fill with gradient
         contextBuffer.fillStyle=grd;
         contextBuffer.fillRect(0,0,canvasWidth,canvasHeight);
-
-        this.image1alpha = 0;
-        this.image2alpha = 0;
-
-        if(game.player.gender==="male")
-        {
-            this.background0.src = "medias/images/homme0.png";
-            this.background1.src = "medias/images/homme1.png";
-            this.background2.src = "medias/images/homme2.png";
-        }
-        if(game.player.gender==="female")
-        {
-            this.background0.src = "medias/images/femme0.png";
-            this.background1.src = "medias/images/femme1.png";
-            this.background2.src = "medias/images/femme2.png";
-        }
     }
     this.drawBackground = function()
     {
-        if(game.player.assurance<50)
-        {
-            this.image1alpha = (game.player.assurance/100)*2;
-            this.image2alpha = 0;
-        }
-        if(game.player.assurance>50)
-        {
-            this.image2alpha = 1-((game.player.assurance/100)*2);
-            this.image1alpha = 0;
-        }
-        console.log(game.player.assurance);
-        context.globalAlpha = 1;
-        context.drawImage(this.background1,0,0);
-        context.globalAlpha = this.image1alpha;
-        context.drawImage(this.background0,0,0);
-        context.globalAlpha = this.image2alpha;
-        context.drawImage(this.background2,0,0);   
-        context.globalAlpha = 1;        
+        context.drawImage(this.background,0,0);       
     }
     this.render = function()
     {       
