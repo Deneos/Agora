dd8d21aa012cee42e0fa21e2e0156abca81166c8
Deneos <christophe.galati@gmail.com>
2014-01-25 17:25:05 +0100

    menu base

---
+++ scripts/managers/hud_methods.js
@@ -0,0 +1,16 @@
+function credits()
+{
+    $("#play").fadeOut();
+    $("#credit").fadeOut();
+    $("#return").fadeIn();
+}
+function returnToMenu()
+{
+    $("#play").fadeIn();
+    $("#credit").fadeIn();
+    $("#return").fadeOut();
+}
+function hideHUD()
+{
+    $("#HUD").fadeOut();
+}
