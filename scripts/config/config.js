00b90313a819da85a1a7e2e523adec35540fbea9
FONTAINE THOMAS <tom_1993@hotmail.fr>
2014-01-25 11:31:07 +0100

    Commit des modifs config/sons

--- scripts/config/config.js
+++ scripts/config/config.js
@@ -5,12 +5,22 @@ var config = {
 		'head' : {
 			'good' : {
 				'sentences' : ["You're marvelous !", "You're a nice people"],
-				'sound'     : ['../../sons/marvelous.mp3', 'sound/nice.mp3']
+				'sound'     : ['medias/sons/Agora_OST_01.mp3', 'sound/nice.mp3']
 			},
 			'bad' : {
 				'sentences' : ["You're fucking ugly !", "You're a fat whore"],
 				'sound'     : ['../../sons/ugly.mp3', 'sound/fat.mp3']
 			}
+		}
+	},
+	'howlerSounds' : {
+		'Agora_OST_01' : {
+			'autoplay' : true,
+			'loop' 	   : true,
+			'volume'   : 0.5,
+			onend: function() {
+		    	console.log("Sound Finished !");
+		  }
 		}
 	}
 };
